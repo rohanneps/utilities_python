ab96573f3034d34193f84850ea905ec90630f3d0
Rohan Man Amatya <rohanneps@gmail.com>
2019-02-28 17:56:18 +0545

    Added command line configuration for website and max_depth

--- python_crawler/start.py
+++ python_crawler/start.py
@@ -1,10 +1,24 @@
 import os
-from core.crawler import Crawler
+import argparse
+# from core.crawler import Crawler
+from core.crawler_sel import Crawler
 
 
 if __name__=='__main__':
-	home_page = 'https://example.com/'
-	crawler = Crawler(domain = home_page)
-	crawler.start_crawling()
-	# crawl.print_crawled_list()
-	crawler.write_to_file()
+	parser = argparse.ArgumentParser(description='Python Website Crawler upto n-depths')
+	parser.add_argument("--website", help="Full website path here. E.g. https://somethings.com", type=str)
+	parser.add_argument("--max_depth", help="Maximum deptho of crawling", type=int, default=3)
+
+
+	args = parser.parse_args()
+	website = args.website
+	max_depth = args.max_depth
+
+	crawler = Crawler(domain = website, max_depth = max_depth)
+	try:
+		crawler.start_crawling()
+	except:
+		pass
+	
+	crawler.write_to_file()	# file_writer_obj
+	crawler.close_resources()
